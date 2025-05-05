baa446e02bc08b1bad3b2c4b6eb62136c7d49e26
amitcjc <amitcjcpune@gmail.com>
2025-05-05 18:36:11 +0530

    by CJC

--- DemoJavaGit/src/Demo.java
+++ DemoJavaGit/src/Demo.java
@@ -2,6 +2,13 @@
 public class Demo
 {
 
+	public void m1()
+	{
+		System.out.println("This is M1 Method");
+	}
+	
+	
+	
 	public static void main(String[] args)
 	{
 	System.out.println("This is Demo Class");	
